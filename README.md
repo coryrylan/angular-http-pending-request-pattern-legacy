d08dd1b000301fc20f25b25d4dff5631da181d0f
coryrylan <git@stackblitz.com>
2025-06-07 19:22:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-http-pending-request-pattern-legacy
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/coryrylan/angular-http-pending-request-pattern-legacy)
---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { UserService, User, Pending, Status } from './user.service';
+
+@Component({
+  selector: 'app-root',
+  providers: [UserService],
+  imports: [CommonModule, HttpClientModule],
+  template: `
+    <a href="https://coryrylan.com/blog/angular-http-pending-request-pattern">
+      <h1>Tutorial: Angular HTTP Pending Request Pattern</h1>
+    </a>
+
+    <section *ngIf="user.data | async as user">
+      <h3>{{user.name}}</h3>
+      <p>Height: {{user.height}}</p>
+      <p>Mass: {{user.mass}}</p>
+      <p>Homeworld: {{user.homeworld}}</p>
+    </section>
+
+    <section [ngSwitch]="user.status | async">
+      <span *ngSwitchCase="Status.LOADING">Loading User...</span>
+      <span *ngSwitchCase="Status.ERROR">There was an error loading the user.</span>
+    </section>
+  `,
+})
+export class App {
+  readonly Status = Status;
+  readonly user: Pending<User>;
+
+  constructor(private userService: UserService) {
+    this.user = this.userService.load(1);
+  }
+}
+
+bootstrapApplication(App);
